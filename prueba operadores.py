c80c8472c9f281bfd0f43ba0f392b65800101545
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-08-24 12:06:17 +0200

    Create prueba operadores.py

---
+++ prueba operadores.py
@@ -0,0 +1,16 @@
+a = -1
+b = 5.0
+c = 2
+d = 6
+
+print('a =', a)
+print('b =', b)
+print('c =', c)
+print('d =', d)
+
+print('\nPositive/Negative:')
+print('abs(a):', abs(a))
+print('neg(a):', neg(a))
+print('neg(b):', neg(b))
+print('pos(a):', pos(a))
+print('pos(b):', pos(b))
